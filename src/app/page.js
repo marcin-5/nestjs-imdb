12a095a611201058ed0d10a5c5d1822d3d00b031
Marcin Bojara <marcin_5@mail.ru>
2024-01-14 19:20:46 +0100

    fetch data from tmdb api

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <div>Home</div>;
+import Results from '@/components/Results';
+
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || 'popular';
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === 'fetchTrending'
+        ? `trending/all/week`
+        : genre === 'fetchTopRated'
+        ? `movie/top_rated`
+        : 'movie/popular'
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  const data = await res.json();
+  const results = data.results;
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
