1fea3ccacc644cf791a1f07c85eba4226ea5acf8
Marcin Bojara <marcin_5@mail.ru>
2024-01-14 17:49:15 +0100

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,28 @@
+export default function About() {
+  return (
+    <div className="p-3 mx-auto space-y-4 max-w-6xl">
+      <h1 className="text-2xl font-medium text-amber-800 dark:text-amber-500">About</h1>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for all your
+        movie-related needs.
+      </p>
+
+      <p>
+        Our website is designed to provide you with a comprehensive database of movies
+        from all around the world, along with the latest news, reviews, and trailers.
+        Our movie database is constantly updated with new releases, ensuring that you
+        have access to the latest and greatest in the world of cinema. You can search
+        for movies by title, director, actor, genre, or release date, making it easy to
+        find the perfect movie for any occasion.
+      </p>
+
+      <p>
+        Thank you for visiting our website and we hope you enjoy your time browsing
+        through our movie database. If you have any feedback or suggestions, please feel
+        free to contact us. We are always looking for ways to improve and enhance the
+        user experience on our website. Happy browsing!
+      </p>
+    </div>
+  );
+}
